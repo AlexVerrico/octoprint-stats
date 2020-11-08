4f65c15df5d67528fdab9de56f35a8023991caf3
Alex V <contact@alexverrico.com>
2020-11-09 09:00:21 +1100

    Bump version to 3.1.2 (#15)
    * Bump version to 3.1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_stats"
 plugin_name = "OctoPrint-Stats"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "3.1.1"
+plugin_version = "3.1.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
